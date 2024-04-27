8d7f20e67f3393cccb65036e7c6d951a13e2fab0
Mystic3945 <96491511+Mystic3945@users.noreply.github.com>
2024-04-27 16:49:03 -0400

    rfs.sh

--- rfs.sh
+++ rfs.sh
@@ -52,9 +52,9 @@ chown "$username" /usr/local/bin/random_song
 if pgrep -x "nemo" > /dev/null; then
 echo '#!/bin/bash
 gnome-terminal -- /bin/bash -c "/usr/local/bin/random_song"' > /home/"$username"/.local/share/nemo/scripts/Play\ Random\ Song
+fi
 chmod +x /home/"$username"/.local/share/nemo/scripts/Play\ Random\ Song
 chown "$username" /home/"$username"/.local/share/nemo/scripts/Play\ Random\ Song
-fi
 
 clear
 
