50c16a41da65add76509506ba2d696523379a3de
Mystic3945 <96491511+Mystic3945@users.noreply.github.com>
2024-04-30 13:27:53 -0400

    rfs.sh

--- rfs.sh
+++ rfs.sh
@@ -58,7 +58,7 @@ if [ -n "$WSL_DISTRO_NAME" ]; then
 wget "https://raw.githubusercontent.com/Mystic3945/random_song/main/wsl_rightclick.bat"
 cmd.exe /c wsl_rightclick.bat
 windows_info="Or right click inside of any folder and 
-click ${YELLOW}Play Random Song${NOCOLOR}
+click ${YELLOW}Play Random Song${NOCOLOR}"
 fi
 
 # Displayus some info about the script before exiting
