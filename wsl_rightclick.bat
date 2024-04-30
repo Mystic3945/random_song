e30109313912cb87175bc49fd4cd7cef4810b759
Mystic3945 <96491511+Mystic3945@users.noreply.github.com>
2024-04-30 12:06:36 -0400

    Add files via upload

---
+++ wsl_rightclick.bat
@@ -0,0 +1,35 @@
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+
+
+
+
+::wsl --install
+
+::wsl --install -d debian
+
+reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Play Random Song" /f
+
+reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Play Random Song\command" /d "wsl bash /usr/local/bin/random_song" /f
+
